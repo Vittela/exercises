4b23612a3a8be1c2ab9dec90cbdbfba5c1586153
Vittela <vittela.code@gmail.com>
2026-02-25 12:27:34 -0300

    initial commit

---
+++ math-sequences/countdown/script.js
@@ -0,0 +1,7 @@
+// Countdown
+
+let x = Number(prompt("Digit a number"))
+
+for (i = x; i >= 0; i--) {
+  console.log(i)
+}
---
+++ objects/geometric point/script.js
@@ -0,0 +1,16 @@
+// Class Point
+
+function Point(x, y) {
+  this.x = x
+  this.y = y
+}
+
+let coordinates = new Point(2, 4)
+
+Point.prototype.sum = function () {
+  return (
+    this.x + this.y
+  )
+}
+
+console.log(coordinates.sum())
