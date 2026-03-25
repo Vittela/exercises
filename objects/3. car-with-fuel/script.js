2a936d64a2be87f22332cc4499cf6336b6d29de4
--global <joao.silva@run2biz.com>
2026-03-25 11:38:04 -0300

    feat: car-with-fuel exercise added

---
+++ objects/3. car-with-fuel/script.js
@@ -0,0 +1,42 @@
+// Class Car with fuel
+
+function Car(name, x, y, speed, fuel) {
+  this.name = name
+  this.coordinates = { x: x, y: y }
+  this.speed = speed
+  this.fuel = fuel
+}
+
+let celta = new Car("Celta", 0, 0, 1, 6);
+
+console.log(celta)
+
+Car.prototype.move = function (direction) {
+  if (this.fuel < 1) {
+    return ("Your fuel is over!")
+  };
+
+  if (direction === "right") {
+    this.coordinates.x += this.speed;
+  };
+  if (direction === "left") {
+    this.coordinates.x -= this.speed;
+  }
+  if (direction === "top") {
+    this.coordinates.y += this.speed;
+  }
+  if (direction === "down") {
+    this.coordinates.y -= this.speed;
+  }
+
+  this.fuel -= this.speed
+  console.log(`New ${this.name} coordinates: (${this.coordinates.x}, ${this.coordinates.y})`)
+  
+  return (`Fuel: ${this.fuel -= this.speed}L`)
+}
+
+Car.prototype.refuel = function (howMuchFuel) {
+  this.fuel += howMuchFuel;
+
+  return (`Your ${this.name} now have ${this.fuel}L of fuel`)
+}
